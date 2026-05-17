9297ac53ce184ed4c57c3d51b2024d1e70a4888e
Uma Shankar Agarwal <Umanick7@gmail.com>
2026-05-17 20:09:29 +0530

    Build to add the place

--- src/main/java/basics.java
+++ src/main/java/basics.java
@@ -1,32 +1,61 @@
 import files.payloads;
 import io.restassured.RestAssured;
-import static org.hamcrest.Matcher.*;
+import io.restassured.path.json.JsonPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 public class basics {
 
-    public static void main (String [] args){
+    private static final Logger log = LoggerFactory.getLogger(basics.class);
 
-        //    RestAssured.baseURI = “baseURL”;
+    public static void main(String[] args) {
+
+//    RestAssured.baseURI = “baseURL”;
 //    given → all input details ( Parameters , Authorizations ,
 //    when → Submit the API
 //    then → validate the response
+//
+//         Add place --> Update palce with the new address --> get place validate with the new address
 
         RestAssured.useRelaxedHTTPSValidation();
         RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-        // using the post methods --> to add the place
-        given().log().all().queryParam("key", "qaclick123")
+        // Post Place --> using the post methods --> to add the place
+        String addPlaceResponse = given().log().all().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
                 .body(payloads.addPlace())
                 .when().post("maps/api/place/add/json")
                 // Validate the status code , Header (response from the server) , body value
                 .then().log().all().assertThat().statusCode(200)
-                    .body("scope" , equalTo("APP"))
-                    .header("Server", "Apache/2.4.52 (Ubuntu)");
+                .body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.52 (Ubuntu)")
+                .extract().response().asString();
 
+        JsonPath js = new JsonPath(addPlaceResponse);
+        String place_ID = js.getString("place_id"); // path should be start from parent
+
+        // Put Place --> use the place ID to update th eaddress
+
+        String newAddress = "Park Hospital , Gurugaram";
+        given().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body(payloads.putPlace(place_ID, newAddress))
+                .when().put("maps/api/place/update/json")
+                .then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        // GET place --> the response with the new updated address
+
+        String getPlaceResponse = given().log().all().queryParam("key", "qaclick123")
+                .queryParam("place_id", place_ID)
+                .when().get("maps/api/place/get/json")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js2 = new JsonPath(getPlaceResponse);
+        String getResponseAddress = js2.getString("address");
+        Assert.assertEquals(getResponseAddress, newAddress);
     }
-
-
 }
--- src/main/java/files/payloads.java
+++ src/main/java/files/payloads.java
@@ -10,7 +10,7 @@ public class payloads {
                 "    \"lng\": 33.427362\n" +
                 "  },\n" +
                 "  \"accuracy\": 50,\n" +
-                "  \"name\": \"Test on 17th May \",\n" +
+                "  \"name\": \"Test Basics - Rest API Automation \",\n" +
                 "  \"phone_number\": \"(+91) 984 893 1232\",\n" +
                 "  \"address\": \"Learning API Automation\",\n" +
                 "  \"types\": [\n" +
@@ -20,5 +20,14 @@ public class payloads {
                 "  \"website\": \"http://google.com\",\n" +
                 "  \"language\": \"English\"\n" +
                 "}";
+    }
+
+    public static String putPlace(String placeID, String newAddress){
+
+        return "{\n" +
+                "\"place_id\":\""+placeID+"\",\n" +
+                "\"address\":\""+newAddress+"\",\n" +
+                "\"key\":\"qaclick123\"\n" +
+                "}\n";
     }
 }
