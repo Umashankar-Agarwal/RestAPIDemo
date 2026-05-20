076fa9b42cd592c840bf914fa45dac40ba7a81dc
Uma Shankar Agarwal <Umanick7@gmail.com>
2026-05-20 23:32:31 +0530

    Automated to play around OAuthValidation

--- src/main/java/OAuthValidation.java
+++ src/main/java/OAuthValidation.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import files.ReUsablesMethods;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
 
 public class OAuthValidation {
+
+    public static void main(String[] args) {
+
+        useRelaxedHTTPSValidation();
+        baseURI = "https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token";
+
+        String authorizationResponse = given().log().all().formParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .formParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+                .formParam("grant_type", "client_credentials")
+                .formParam("scope", "trust")
+                .when().post()
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js = ReUsablesMethods.jsonConverter(authorizationResponse);
+        String accessToken = js.get("access_token");
+        System.out.println(accessToken);
+
+        String courseDetailsResponse = given().queryParam("access_token", accessToken)
+                .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+        JsonPath js1 = ReUsablesMethods.jsonConverter(courseDetailsResponse);
+
+        System.out.println(courseDetailsResponse);
+    }
 }
