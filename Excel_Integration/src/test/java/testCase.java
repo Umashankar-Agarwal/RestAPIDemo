6158ed0d1da1bf0a1a0b15663018bf6cf6d5dda6
Uma Shankar Agarwal <Umanick7@gmail.com>
2026-06-10 14:48:34 +0530

    added the graphQlProject

---
+++ Excel_Integration/src/test/java/testCase.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class testCase {
+
+    public static void main (String [] args ) throws IOException {
+        dataDriven d = new dataDriven();
+
+        ArrayList<String> data = d.getData("Sample1", "Purchase");
+        System.out.println(data.get(0));
+        System.out.println(data.get(1));
+        System.out.println(data.get(2));
+        System.out.println(data.get(3));
+    }
+}
